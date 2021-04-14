908580cd13f1a898a2c6c6d4669018898f5acfcc
Dean Cheukalam <dean.cheukalam@hakrinbank.com>
2021-04-14 13:36:38 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Inventorious.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
